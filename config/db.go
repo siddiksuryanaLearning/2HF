0dc3ad2538019626a8c2b5a5b68a4e633828a31b
Programmer <36xitkj1@gmial.com>
2023-08-08 00:09:25 +0700

    update databases lagi

--- config/db.go
+++ config/db.go
@@ -38,10 +38,10 @@ func ConnectDataBase() *gorm.DB {
 	} else {
 
 		username := "root"
-		password := ""
+		password := "password"
 		// host := "tcp(192.168.217.88:3306)"
 		// host := "tcp(localhost:3306)"
-		host := "tcp(localhost:3306)"
+		host := "tcp(db2hf.cbw2vfbfmfv7.us-east-1.rds.amazonaws.com:3306)"
 		database := "db2hf"
 
 		dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
